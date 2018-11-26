f672af7ddfa5f66d4a3df86f7d1236998137d8a6
Bon-gyu, KOO <freestyle@nexell.co.kr>
2018-11-26 16:27:29 +0900

    pwm: nexell: do not set unused channels at device tree
    This patch provides allows user to selectively supply the tclk clock.
    Set the channel to be used and the clk frequency pair.
    For more information, see Documentation.
    
    Change-Id: I4459902bf59082ddfb01a422c81ff167e293464f
    Signed-off-by: Bon-gyu, KOO <freestyle@nexell.co.kr>

--- drivers/pwm/pwm-nxp3220.c
+++ drivers/pwm/pwm-nxp3220.c
@@ -402,7 +402,12 @@ static int pwm_nexell_parse_dt(struct nexell_pwm_chip *nx_pwm)
 		if (ch >= PWM_NUM)
 			break;
 
-		nx_pwm->freq[ch++] = val;
+		for ( ; ch < PWM_NUM; ch++) {
+			if (nx_pwm->output_mask & BIT(ch)) {
+				nx_pwm->freq[ch++] = val;
+				break;
+			}
+		}
 	}
 
 	return 0;
@@ -449,6 +454,9 @@ static int pwm_nexell_probe(struct platform_device *pdev)
 	}
 
 	for (ch = 0; ch < PWM_NUM; ++ch) {
+		if (!(nx_pwm->output_mask & BIT(ch)))
+			continue;
+
 		nx_pwm->tclk[ch] = devm_clk_get(dev, pwm_tclk_name[ch]);
 		if (IS_ERR(nx_pwm->tclk[ch])) {
 			dev_err(dev, "failed to get pwm tclk %d\n", ch);
@@ -466,11 +474,9 @@ static int pwm_nexell_probe(struct platform_device *pdev)
 			dev_err(dev, "failed to enable clock %d\n", ch);
 			return ret;
 		}
-	}
 
-	for (ch = 0; ch < PWM_NUM; ++ch)
-		if (nx_pwm->output_mask & BIT(ch))
-			pwm_nexell_set_invert(nx_pwm, ch, true);
+		pwm_nexell_set_invert(nx_pwm, ch, true);
+	}
 
 	platform_set_drvdata(pdev, nx_pwm);
 
@@ -478,15 +484,16 @@ static int pwm_nexell_probe(struct platform_device *pdev)
 	if (ret < 0) {
 		dev_err(dev, "failed to register PWM nx_pwm\n");
 		for (ch = 0; ch < PWM_NUM; ++ch)
-			clk_disable_unprepare(nx_pwm->tclk[ch]);
+			if (nx_pwm->output_mask & BIT(ch))
+				clk_disable_unprepare(nx_pwm->tclk[ch]);
 		return ret;
 	}
 
 	for (ch = 0; ch < PWM_NUM; ch++) {
 		dev_dbg(dev, "tclk%d at %lu\n",
 				ch,
-				!IS_ERR(nx_pwm->tclk[ch]) ?
-				clk_get_rate(nx_pwm->tclk[ch]) : 0);
+				IS_ERR_OR_NULL(nx_pwm->tclk[ch]) ? 0 :
+				clk_get_rate(nx_pwm->tclk[ch]));
 	}
 
 	return 0;
@@ -503,7 +510,8 @@ static int pwm_nexell_remove(struct platform_device *pdev)
 		return ret;
 
 	for (ch = 0; ch < PWM_NUM; ++ch)
-		clk_disable_unprepare(nx_pwm->tclk[ch]);
+		if (nx_pwm->output_mask & BIT(ch))
+			clk_disable_unprepare(nx_pwm->tclk[ch]);
 
 	return 0;
 }
@@ -518,6 +526,9 @@ static int pwm_nexell_suspend(struct device *dev)
 	for (ch = 0; ch < PWM_NUM; ++ch) {
 		struct pwm_device *pwm = &chip->pwms[ch];
 
+		if (!(nx_pwm->output_mask & BIT(ch)))
+			continue;
+
 		/* save registers */
 		nx_pwm->tcfg0[ch] = readl(nx_pwm->base + REG_TCFG0(ch));
 		nx_pwm->tcfg1[ch] = readl(nx_pwm->base + REG_TCFG1(ch));
@@ -528,7 +539,7 @@ static int pwm_nexell_suspend(struct device *dev)
 		pwm_nexell_disable(chip, pwm);
 
 		/* clock off */
-		if (!IS_ERR(nx_pwm->tclk[ch]))
+		if (!IS_ERR_OR_NULL(nx_pwm->tclk[ch]))
 			clk_disable_unprepare(nx_pwm->tclk[ch]);
 	}
 
@@ -545,8 +556,11 @@ static int pwm_nexell_resume(struct device *dev)
 		struct pwm_device *pwm = &chip->pwms[ch];
 		struct nexell_pwm_channel *nx_chan = pwm_get_chip_data(pwm);
 
+		if (!(nx_pwm->output_mask & BIT(ch)))
+			continue;
+
 		/* clock on */
-		if (!IS_ERR(nx_pwm->tclk[ch]))
+		if (!IS_ERR_OR_NULL(nx_pwm->tclk[ch]))
 			clk_prepare_enable(nx_pwm->tclk[ch]);
 
 		/* restore registers */
