85e348541fb705458be1afd832589bccffb44677
jhkim <jhkim@nexell.co.kr>
2019-08-27 10:29:21 +0900

    mach: nxp3220: change 'Hardware' name in '/proc/cpufino'
    This patch change 'Hardware' name from nexell to nxp3220 in '/proc/cpufino'
    to distingush other cpu series.
    
    $> cat /proc/cpuinfo
     ....
     Hardware        : nxp322x (Device Tree Support)
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: Ice5911df91461690a6a3037c8fb0b0f2517787b3
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/linux_kernel-4.14.x/+/466352

--- arch/arm/mach-nexell/cpu.c
+++ arch/arm/mach-nexell/cpu.c
@@ -12,6 +12,10 @@ static const char * const nexell_dt_compat[] = {
 /*------------------------------------------------------------------------------
  * Maintainer: Nexell Co., Ltd.
  */
+#ifdef CONFIG_ARCH_NXP3220_COMMON
+DT_MACHINE_START(NEXELL_DT, "nxp322x (Device Tree Support)")
+#else
 DT_MACHINE_START(NEXELL_DT, "NEXELL (Device Tree Support)")
+#endif
 	.dt_compat	= nexell_dt_compat,
 MACHINE_END
