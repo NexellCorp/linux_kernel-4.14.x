f56d8fee5301d0cea99806e2bbc4b401d93a27c3
Hyunseok Jung <hsjung@nexell.co.kr>
2020-01-07 17:37:46 +0900

    usb: dwc2: edit bus suspend/resume for nxp3220
    This patch edit bus suspend/resume for nxp3220.
    - do not turn off PHY when entering suspend.
    - can enable suspend using g-con_susp bool property
    - register dump process move hcd to platform driver
    - return busy state if you try suspend when usb device is connected.
    
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>
    Change-Id: I40271563223b63bfc76876137fe9edd90dc6ac32
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/linux_kernel-4.14.x/+/479496
    Reviewed-by: jung hyun kim <jhkim@nexell.co.kr>
    Reviewed-by: Eric.park <pjsin865@nexell.co.kr>
    Tested-by: Eric.park <pjsin865@nexell.co.kr>

--- drivers/usb/dwc2/gadget.c
+++ drivers/usb/dwc2/gadget.c
@@ -4718,6 +4718,17 @@ int dwc2_hsotg_suspend(struct dwc2_hsotg *hsotg)
 	if (hsotg->lx_state != DWC2_L0)
 		return 0;
 
+	if (of_device_is_compatible(hsotg->dev->of_node,
+		 "nexell,nxp3220-dwc2otg")
+		&& !hsotg->params.g_con_susp) {
+		u32 usb_status = readl(hsotg->regs + GOTGCTL);
+
+		if (usb_status & GOTGCTL_BSESVLD) {
+			dev_warn(hsotg->dev, "usb device is still connected\n");
+			return -EBUSY;
+		}
+	}
+
 	if (hsotg->driver) {
 		int ep;
 
