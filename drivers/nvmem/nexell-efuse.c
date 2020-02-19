0dc2758157d9de08c837a7ae0a48c96c2ad86527
jhkim <jhkim@nexell.co.kr>
2020-02-19 12:12:14 +0900

    nvmem: nexell-efuse: add nexell nvmem efuse driver
    This patch add nexell nvmem efuse driver
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I9959d893631048079acc93a8b47aceaf9a05a96f
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/linux_kernel-4.14.x/+/485179

---
+++ drivers/nvmem/nexell-efuse.c
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2018  Nexell Co., Ltd.
+ * Author: JungHyun, Kim <jhkim@nexell.co.kr>
+ */
+#include <linux/clk.h>
+#include <linux/device.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/nvmem-provider.h>
+#include <linux/of.h>
+#include <linux/of_platform.h>
+#include <linux/platform_device.h>
+#include <linux/soc/nexell/sec_io.h>
+
+#define EFUSE_WORD_SIZE		4
+#define EFUSE_STRIDE		4
+
+struct nx_efuse_priv {
+	struct device *dev;
+	dma_addr_t base;
+	void __iomem *addr;
+	struct clk *clk;
+	const char *name;
+	int id;
+	bool is_secure;
+	struct nvmem_config config;
+};
+
+static int nx_efuse_read(void *context, unsigned int offset,
+				      void *val, size_t bytes)
+{
+	struct nx_efuse_priv *efuse = context;
+	struct nvmem_config *config = &efuse->config;
+	void __iomem *addr;
+	u32 *buf = val;
+	int size, i, ret;
+
+	if (!IS_ALIGNED(offset, EFUSE_WORD_SIZE) ||
+	    !IS_ALIGNED(bytes, EFUSE_WORD_SIZE)) {
+		dev_err(efuse->dev, "cell %s unaligned to word size %d\n",
+			config->name, config->word_size);
+		return -EINVAL;
+	}
+
+	if (offset + bytes > config->size) {
+		dev_err(efuse->dev,
+			"cell %s request 0x%x:0x%x over to size %d\n",
+			config->name, offset, offset + bytes, config->size);
+		return -EINVAL;
+	}
+
+	if (!IS_ERR(efuse->clk)) {
+		ret = clk_prepare_enable(efuse->clk);
+		if (ret < 0) {
+			dev_err(efuse->dev,
+				"cell %s failed to enable efuse clk\n",
+				config->name);
+			return ret;
+		}
+	}
+
+	addr = efuse->is_secure ? (void __iomem *)efuse->base : efuse->addr;
+	addr = addr + offset;
+	size = bytes > config->size ? config->size : bytes;
+
+	for (i = 0; i < (size / EFUSE_WORD_SIZE); i++)
+		buf[i] = efuse->is_secure ?
+			 sec_readl(addr + (i * EFUSE_WORD_SIZE)) :
+			 readl(addr + (i * EFUSE_WORD_SIZE));
+
+	if (!IS_ERR(efuse->clk))
+		clk_disable_unprepare(efuse->clk);
+
+	return size;
+}
+
+static int nx_efuse_probe(struct platform_device *pdev)
+{
+	struct resource *res;
+	struct nvmem_device *nvmem;
+	struct nvmem_config *config;
+	struct nx_efuse_priv *efuse;
+	struct device *dev = &pdev->dev;
+
+	efuse = devm_kzalloc(dev, sizeof(struct nx_efuse_priv),
+			     GFP_KERNEL);
+	if (!efuse)
+		return -ENOMEM;
+
+	efuse->is_secure = of_property_read_bool(dev->of_node, "cell-secure");
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (efuse->is_secure) {
+		efuse->addr = devm_ioremap_resource(&pdev->dev, res);
+		if (IS_ERR(efuse->addr))
+			return PTR_ERR(efuse->addr);
+	}
+
+	efuse->clk = devm_clk_get(dev, NULL);
+	efuse->name = of_get_property(dev->of_node, "cell-name", NULL);
+	of_property_read_s32(dev->of_node, "cell-id", &efuse->id);
+
+	efuse->base = res->start;
+	efuse->dev = dev;
+
+	config = &efuse->config;
+	config->name = efuse->name;
+	config->id = efuse->id;
+	config->priv = efuse;
+	config->dev = efuse->dev;
+	config->owner = THIS_MODULE;
+	config->read_only = true;
+	config->reg_read = &nx_efuse_read;
+	config->size = resource_size(res);
+	config->word_size = EFUSE_WORD_SIZE;
+	config->stride = EFUSE_STRIDE;
+
+	nvmem = nvmem_register(config);
+	if (IS_ERR(nvmem))
+		return PTR_ERR(nvmem);
+
+	platform_set_drvdata(pdev, nvmem);
+
+	return 0;
+}
+
+static int nx_efuse_remove(struct platform_device *pdev)
+{
+	struct nvmem_device *nvmem = platform_get_drvdata(pdev);
+
+	return nvmem_unregister(nvmem);
+}
+
+static const struct of_device_id nx_efuse_match[] = {
+	{ .compatible = "nexell,nxp3220-efuse", },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, nx_efuse_match);
+
+static struct platform_driver nx_efuse_driver = {
+	.probe = nx_efuse_probe,
+	.remove = nx_efuse_remove,
+	.driver = {
+		.name = "nexell-efuse",
+		.of_match_table = nx_efuse_match,
+	},
+};
+
+static int __init nx_efuse_init(void)
+{
+	return platform_driver_register(&nx_efuse_driver);
+}
+
+static void __exit nx_efuse_exit(void)
+{
+	platform_driver_unregister(&nx_efuse_driver);
+}
+
+subsys_initcall(nx_efuse_init);
+module_exit(nx_efuse_exit);
+
+MODULE_DESCRIPTION("Nexell efuse driver");
+MODULE_LICENSE("GPL v2");
