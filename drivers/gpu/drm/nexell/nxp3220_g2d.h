a23bafa2e28b39377e15ace412262f9f1d8b58d9
jhkim <jhkim@nexell.co.kr>
2020-03-26 11:25:08 +0900

    DRM: G2D: Add wait sync ioctl
    This patch add wait sync ioctl
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I405105521e7ab05faf26a2eb343137f0d6ca6dc5

--- drivers/gpu/drm/nexell/nxp3220_g2d.h
+++ drivers/gpu/drm/nexell/nxp3220_g2d.h
@@ -10,11 +10,14 @@
 #define NX_G2D_DRIVER_VER_MAJOR		1
 #define NX_G2D_DRIVER_VER_MINOR		1
 
-int nx_drm_g2d_get_version(struct drm_device *drm_dev, void *data,
+int nx_g2d_get_version(struct drm_device *drm_dev, void *data,
+		       struct drm_file *file);
+int nx_g2d_exec_ioctl(struct drm_device *drm_dev, void *data,
+		      struct drm_file *file);
+int nx_g2d_sync_ioctl(struct drm_device *drm_dev, void *data,
+		      struct drm_file *file);
+int nx_g2d_exec_sync_ioctl(struct drm_device *drm_dev, void *data,
 			   struct drm_file *file);
-int nx_drm_g2d_exec_ioctl(struct drm_device *drm_dev, void *data,
-			  struct drm_file *file);
-int nx_drm_g2d_sync_ioctl(struct drm_device *drm_dev, void *data,
-			  struct drm_file *file);
+
 #endif
 
